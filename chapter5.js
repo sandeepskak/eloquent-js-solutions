bc6dde3232aa5246d9d360bb79bb36accc5dd009
sandeep <653sandeep@gmail.com>
2016-11-24 19:04:44 +0530

    Remove unused

--- chapter5.js
+++ chapter5.js
@@ -27,31 +27,12 @@ function average(array) {
   return array.reduce(plus) / array.length;
 }
 
-
 //Exercise 3
 ancestry = JSON.parse(ANCESTRY_FILE);
-var byName = {};
-ancestry.forEach(function(person) {
-  byName[person.name] = person;
-});
-
-centuryAgeMap = ancestry.map(function(person) {
-  return {century: Math.ceil(person.died / 100), age: (person.died - person.born)};
-});
-//[{century: 16, age: 70}, {century: 20, age: 80}]
-
-centuries = ancestry.map(function(person) {
-  return Math.ceil(person.died / 100);
-});
-
-uniqueCenturies = centuries.filter((value, index, self) => self.indexOf(value) === index);
-//[20, 18, 19, 16, 21, 17]
-
 var byDied = {};
 ancestry.forEach(function(person) {
   byDied[person.died] = (person.born - person.died);
 });
-
 
 var years = Object.keys(byDied);
 
