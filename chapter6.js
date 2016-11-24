12bd32d83095ace0c6f8c037f1be44c588092b8d
sandeep <653sandeep@gmail.com>
2016-11-24 20:27:14 +0530

    Chapter 6 - exercise 2

--- chapter6.js
+++ chapter6.js
@@ -20,3 +20,22 @@ Object.defineProperty(Vector.prototype, 'length', {
   get: function() {
   return Math.sqrt(this.x * this.x + this.y * this.y);
 }});
+
+//Exercise 2
+function StretchCell(inner, width, height) {
+  this.inner = inner;
+  this.width = width;
+  this.height = height;
+}
+
+StretchCell.prototype.minWidth = function() {
+  return Math.max(this.width, this.inner.minWidth());
+};
+
+StretchCell.prototype.minHeight = function() {
+  return Math.max(this.height, this.inner.minHeight());
+};
+
+StretchCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height);
+};
