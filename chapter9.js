793165325e231b451aaa472a173453a1967f77db
sandeep <653sandeep@gmail.com>
2016-11-28 14:33:54 +0530

    Chapter 9 - ex2

--- chapter9.js
+++ chapter9.js
@@ -41,3 +41,11 @@ function verify(regexp, yes, no) {
       console.log("Unexpected match for '" + s + "'");
   });
 }
+// Exercise 2
+
+var text = "'I'm the cook,' he said, 'it's my job.'";
+
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
+// → "I'm the cook," he said, "it's my job."
+
+//Exercise 3
